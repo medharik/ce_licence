134c3409caaac71442925d1a4236befc29d6834b
unknown <med.harik@gmail.com>
2019-12-25 20:27:50 +0000

    POO : encapsulation et les constructeur

--- poo_classe/Produit.java
+++ poo_classe/Produit.java
@@ -1,12 +1,72 @@
 package poo_classe;
 
 public class Produit {
+	//Principe numero 1 de la poo :
+	// encapsultion :
+	// role principal : proteger les donnees des objets 
+	//comment : on les declarant private et on n'autorisant leurs acces qu'a travers 
+	//des setters (mutateurs pour l'ecriture) et des gettes (accesseurs pour la lecture)
 	//attributs <=> fields
-	String libelle;
-	double prix;
-	double qtestock;
- 
-	//methodes 
+
+//constructeurs : 
+	// des  methodes permettantd'initialiser les attributs (lors de l'appel de new )
+private 	String libelle;
+private 	double prix;
+private 	double qtestock;
+
+//constructeur
+public Produit() {
+
+}
+
+public Produit(String libelle,  double prix , double qteStock ) {
+	this.libelle=libelle;
+	this.prix=prix;
+	this.qtestock=qteStock;
+}
+public Produit(Produit p){
+this(p.libelle, p.prix, p.qtestock);
+
+}
+
+public Produit(String libelle, double prix) {
+	
+	this.libelle = libelle;
+	this.prix = prix;
+}
+
+//ecriture le prix
+public void setPrix(double prix) {
+	if(prix>=0)
+this.prix = prix;
+	else 
+		System.out.println("prix incorrect");
+}
+//lecture du prix
+public double getPrix() {
+	return prix;
+}
+
+	public String getLibelle() {
+		if(this.libelle==null)
+			return "Inconnu";
+		else
+	return libelle.toUpperCase();
+}
+public void setLibelle(String libelle) {
+this.libelle=libelle;
+}
+public double getQtestock() {
+	return qtestock;
+}
+public void setQtestock(double qtestock) throws Exception {
+	if (qtestock >=0) {
+	this.qtestock = qtestock;	
+	}else
+		throw new Exception("Erreur ,car  la qte en stock est < 0");
+	
+}
+	//methodes  
 	void afficher() {
 	System.out.println("-----------------------------");
 	System.out.println("libelle est "+libelle+" , prix : "+prix);
--- poo_classe/Test.java
+++ poo_classe/Test.java
@@ -2,24 +2,42 @@ package poo_classe;
 
 public class Test {
 public static void main(String[] args) {
-	//declarer un objet de type Produit
-	Produit hp ;
-	//instancier un objet de type Produit : hp
-	hp = new Produit();
+//	//declarer un objet de type Produit
+//	Produit hp ;
+//	//instancier un objet de type Produit : hp
+//	hp = new Produit();
+//	
+//	hp.libelle="hp dv 7";
+//	hp.prix=9000;
+//	hp.qtestock=10;
+//	
+//	hp.afficher();
+//	hp.entreeStock(30);
+//	hp.afficher();
+//	double tht= hp.vendre(25);
+//	System.out.println("tht est "+tht);
+//	hp.afficher();
+//	//un autre
+//	Produit dell =new Produit();
+//	dell.libelle=" dell satelite ";
+//	dell.afficher();
+	//
+	Produit sony =new Produit();
+	sony.afficher();
+	sony.setPrix(1000);
+	try {
+		sony.setQtestock(10);
+		sony.afficher();
+		sony.setLibelle("sony vaio");
+	} catch (Exception e) {
+		
+	System.out.println(e.getMessage());
+	}
+	System.out.println("le prix  de  "+sony.getLibelle()+" est "+sony.getPrix());
 	
-	hp.libelle="hp dv 7";
-	hp.prix=9000;
-	hp.qtestock=10;
-	
-	hp.afficher();
-	hp.entreeStock(30);
-	hp.afficher();
-	double tht= hp.vendre(25);
-	System.out.println("tht est "+tht);
-	hp.afficher();
-	//un autre
-	Produit dell =new Produit();
-	dell.libelle=" dell satelite ";
-	dell.afficher();
+	Produit mac=new Produit("Mac pro", 10000 ,10);
+	mac.afficher();
+	Produit  sony2=new Produit(sony);
+sony2.afficher();
 }
 }
